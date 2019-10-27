1761535f9ec1273947621836f69f118d99486644
Lex Nastin <32967396+ArchGryphon9362@users.noreply.github.com>
2019-10-27 17:36:54 +0000

    Add this to all your installer SFXs #fixed
    Fixed Version!

--- ATCtweak.cmd
+++ ATCtweak.cmd
@@ -5,38 +5,38 @@ CD >NUL
 
 SET KEY=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ATC
 
-REG ADD %KEY% /v DisplayName /d "Aww That's Cube"
+REG ADD %KEY% /v DisplayName /d "Aww That's Cube" /f
 
-REG ADD %KEY% /v DisplayIcon /d "%CD%\Aww That's Cube.exe"
+REG ADD %KEY% /v DisplayIcon /d "%CD%\Aww That's Cube.exe" /f
 
-REG ADD %KEY% /v UninstallString /d ATCunins.cmd
+REG ADD %KEY% /v UninstallString /d %UserProfile%\AppData\Roaming\ATCunins.cmd /f
 
-REG ADD %KEY% /v Publisher /d "ManuelMines"
+REG ADD %KEY% /v Publisher /d "ManuelMines" /f
 
-REG ADD %KEY% /v NoModify /t REG_DWORD /d 1
+REG ADD %KEY% /v NoModify /t REG_DWORD /d 1 /f
 
-REG ADD %KEY% /v NoRepair /t REG_DWORD /d 1
+REG ADD %KEY% /v NoRepair /t REG_DWORD /d 1 /f
 
 :: Creating ATCunins.cmd -------------------------------------------
 
-echo @echo off>%SystemRoot%\ATCunins.cmd
+echo @echo off>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
-echo CD %CD%>>%SystemRoot%\ATCunins.cmd
+echo CD %CD%>>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
-echo :: Deleting ATC directory from Program Files>>%SystemRoot%\ATCunins.cmd
-echo RD /S /Q "%CD%">>%SystemRoot%\ATCunins.cmd
+echo :: Deleting ATC directory from Program Files>>%UserProfile%\AppData\Roaming\ATCunins.cmd
+echo RD /S /Q "%CD%">>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
-echo :: Deleting ATC shortcut folder from Start Menu>>%SystemRoot%\ATCunins.cmd
-echo RM "%%UserProfile%%\Start Menu\Programs\Aww That's Cube.lnk">>%SystemRoot%\ATCunins.cmd
+echo :: Deleting ATC shortcut folder from Start Menu>>%UserProfile%\AppData\Roaming\ATCunins.cmd
+echo RM "%%UserProfile%%\Start Menu\Programs\Aww That's Cube.lnk">>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
-echo :: Deleting ATC shortcut from Desktop>>%SystemRoot%\ATCunins.cmd
-echo RM "%%UserProfile%%\Desktop\Aww That's Cube.lnk">>%SystemRoot%\ATCunins.cmd
+echo :: Deleting ATC shortcut from Desktop>>%UserProfile%\AppData\Roaming\ATCunins.cmd
+echo RM "%%UserProfile%%\Desktop\Aww That's Cube.lnk">>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
-echo :: Deleting ATC uninstall strings from the Registry>>%SystemRoot%\ATCunins.cmd
-echo REG DELETE HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ATC /f>>%SystemRoot%\ATCunins.cmd
+echo :: Deleting ATC uninstall strings from the Registry>>%UserProfile%\AppData\Roaming\ATCunins.cmd
+echo REG DELETE HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ATC /f>>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
-echo :: Deleting ATCunins.cmd (oh! that's me! OOPS!)>>%SystemRoot%\ATCunins.cmd
-echo del %%0>>%SystemRoot%\ATCunins.cmd
+echo :: Deleting ATCunins.cmd (oh! that's me! OOPS!)>>%UserProfile%\AppData\Roaming\ATCunins.cmd
+echo del %%0>>%UserProfile%\AppData\Roaming\ATCunins.cmd
 
 :: Deleting ATCtweak.cmd (oh! that's me! OOPS!) --------------------
 
