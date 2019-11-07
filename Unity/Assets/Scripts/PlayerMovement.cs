c0593935b24a7e925ff0cc59747cfda08f14fc11
ManuelMines <55859604+ManuelMines@users.noreply.github.com>
2019-11-07 23:10:48 +0000

    Arrow Keys Now Work!
    @archgryphon9362 has given me code to make arrow keys now work and you can even now change the key binds just before you launch the game in that window, but the movement is now broken so ill have to adjust that, that's why I’m not making a release right now.

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,29 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
     public GameManager gamemananger;
-    public float forwardForce = 2000f;
-    public float sidewayForce = 1500f;
+    public float forwardForce = 8000f;
+    public float sidewayForce = 300f;
+    public float resistance = 3f;
     public PlayerMovement movement;
 
+    void Start()
+    {
+        Mathf.Clamp(rb.position.y, -100.0f, 0.5f);
+    }
     // We marked this as "FixedUpdate"
+
     void FixedUpdate()
     {
+        {
+            rb.drag = resistance;
+        }
       //  Debug.Log(player.forwardForce);
         //add a forward 
        {
        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
        }
 
-         if ( Input.GetKey("d") )
-       {
-         rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-       } 
-       
-              if (Input.GetKey("a") )
-            {
-              rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-            }
+        rb.AddForce(Input.GetAxis("Horizontal") * sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         if (rb.position.y < -1f)
         {
             movement.enabled = false;
