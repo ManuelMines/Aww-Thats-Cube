c47f6ad5c572065841989b572c3e4648b9e2e4ad
ManuelMines <manuelminescraft@gmail.com>
2020-03-31 01:07:28 +0100

    Bear with me
    I have decided to fully re-make this game. I am having second thoughts about scrapping the original game concept and making some sort of train game, maybe with a similar concept. The folder New unity project is the new folder where ill be storing unity engine files, ill soon remove the original unity folder. this will most likely happen when I have decided the faith of this game.

---
+++ New Unity Project/Assets/Scripts/MainMenu/OnStartup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OnStartup : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+}
